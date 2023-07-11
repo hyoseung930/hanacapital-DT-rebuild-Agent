1abea9e1ca933acb030ff60c62c7ce4594789907
hyoseung930 <35983843+hyoseung930@users.noreply.github.com>
2023-07-12 01:32:12 +0900

    중간수정 업로드

--- src/views/agent/Agent_P00_l002.vue
+++ src/views/agent/Agent_P00_l002.vue
@@ -6,6 +6,12 @@ import PopupTitle from '@/components_agent/ui/layer/PopupTitle.vue';
 import PopupButton from '@/components_agent/ui/layer/PopupButton.vue';
 import ModalPopup from '@/components_agent/ui/layer/ModalPopup.vue';
 import ModalPopupHead from '@/components_agent/ui/layer/ModalPopupHead.vue';
+import BasicButton from '@/components_agent/ui/button/BasicButton.vue';
+import ButtonList from '@/components_agent/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components_agent/ui/button/ButtonListItem.vue';
+
+import IconImg from '@/assets_agent/images/icon/img.svg?component';
+import IconImgColor from '@/assets_agent/images/icon/img-color.svg?component';
 
 export default {
   components: {
@@ -14,6 +20,11 @@ export default {
     PopupButton,
     ModalPopup,
     ModalPopupHead,
+    BasicButton,
+    ButtonList,
+    ButtonListItem,
+    IconImg,
+    IconImgColor,
   },
   setup() {
     const layer = ref(null);
@@ -36,86 +47,112 @@ export default {
           <PopupTitle>서류등록</PopupTitle>
         </ModalPopupHead>
       </template>
-      <section>
-        <div class="container pb80">
-          <table class="table-type-04 none-search-small bt">
-            <tbody>
-              <tr>
-                <td>등록된 게시글이 없습니다.</td>
-              </tr>
-            </tbody>
-          </table>
-
-          <div class="file-send">
-            <div class="flex-container jcsb">
-              <div class="flex-container jcfs">
-                <i class="ico ico-image-no"></i>
-                파일첨부
-              </div>
-              <i class="ico ico-plus-gray"></i>
+      <div class="container pb80">
+        <table class="table-type-04 none-search-small bt">
+          <tbody>
+          <tr>
+            <td>등록된 게시글이 없습니다.</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <ul class="reset-list">
+        <li class="row-margin-container-medium">
+          <div :class="[$style['upload-button'], 'row-margin-contents']">
+            <input
+              type="file"
+              id="IF_P05_l001_upload001"
+              :class="$style['upload-button__input']"
+            />
+            <div :class="$style['upload-button__block']">
+              <span :class="$style['upload-button__img']">
+                <IconImg />
+              </span>
+              <label
+                for="IF_P05_l001_upload001"
+                :class="$style['upload-button__label']"
+                >파일첨부</label
+              >
             </div>
           </div>
 
-          <div class="scroll-container over-w mxh-350">
-            <div class="file-list">
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+          <div :class="[$style['upload-file'], $style['upload-file--inside']]">
+            <ul :class="$style['upload-file__list']">
+              <li :class="$style['upload-file__item']">
+                <div :class="$style['upload-file__icon']">
+                  <IconImgColor />
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+                <div :class="$style['upload-file__content']">
+                  <div :class="$style['upload-file__name']">첨부파일명.jpg</div>
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+                <div :class="$style['upload-file__button']">
+                  <BasicButton line="true" theme="quaternary" size="small">
+                    삭제
+                  </BasicButton>
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+              </li>
+              <li :class="$style['upload-file__item']">
+                <div :class="$style['upload-file__icon']">
+                  <IconImgColor />
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+                <div :class="$style['upload-file__content']">
+                  <div :class="$style['upload-file__name']">첨부파일명.png</div>
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-              <div class="flex-container jcsb">
-                <div class="flex-container jcfs">
-                  <i class="ico ico-image"></i>
-                  파일명.jpg
+                <div :class="$style['upload-file__button']">
+                  <BasicButton line="true" theme="quaternary" size="small">
+                    삭제
+                  </BasicButton>
                 </div>
-                <button class="btn btn-s05">삭제</button>
-              </div>
-            </div>
+              </li>
+              <li :class="$style['upload-file__item']">
+                <div :class="$style['upload-file__icon']">
+                  <IconImgColor />
+                </div>
+                <div :class="$style['upload-file__content']">
+                  <div :class="$style['upload-file__name']">첨부파일명.gif</div>
+                </div>
+                <div :class="$style['upload-file__button']">
+                  <BasicButton line="true" theme="quaternary" size="small">
+                    삭제
+                  </BasicButton>
+                </div>
+              </li>
+              <li :class="$style['upload-file__item']">
+                <div :class="$style['upload-file__icon']">
+                  <IconImgColor />
+                </div>
+                <div :class="$style['upload-file__content']">
+                  <div :class="$style['upload-file__name']">
+                    첨부파일명_첨부파일명_첨부파일명_첨부파일명_첨부파일명.jpeg
+                  </div>
+                </div>
+                <div :class="$style['upload-file__button']">
+                  <BasicButton line="true" theme="quaternary" size="small">
+                    삭제
+                  </BasicButton>
+                </div>
+              </li>
+            </ul>
           </div>
+        </li>
+      </ul>
 
-          <div class="explan mt40">
-            <p>
-              서류등록 파일은 이미지 파일만 가능합니다. (jpg, jpge, png, gif, tif,
-              pdf 파일)
-            </p>
-            <p>서류등록 파일은 최대 5개까지 등록 가능합니다.</p>
-          </div>
+      <div class="explan mt40">
+        <p>
+          서류등록 파일은 이미지 파일만 가능합니다. (jpg, jpge, png, gif, tif,
+          pdf 파일)
+        </p>
+        <p>서류등록 파일은 최대 5개까지 등록 가능합니다.</p>
+      </div>
 
-          <div class="btn-container fixed">
-            <button class="btn btn-s01 btn-search">저장</button>
-          </div>
-        </div>
-      </section>
+      <div class="btn-container fixed">
+        <button class="btn btn-s01 btn-search">저장</button>
+      </div>
     </ModalPopup>
   </UiLayer>
 </template>
+
+<style lang="scss" module>
+@import '@/assets_agent/scss/components/import/upload-button.scss';
+@import '@/assets_agent/scss/components/import/upload-file.scss';
+</style>
--- src/views/agent/Agent_P02_l003.vue
+++ src/views/agent/Agent_P02_l003.vue
@@ -36,51 +36,59 @@ export default {
           <PopupTitle>상환스케줄</PopupTitle>
         </ModalPopupHead>
       </template>
-      <section>
-        <div class="container">
-          <div class="title">계약내용</div>
-          <table class="table-type-01">
-            <colgroup>
-              <col width="13%" />
-              <col width="20%" />
-              <col width="13%" />
-              <col width="20%" />
-              <col width="13%" />
-              <col width="20%" />
-            </colgroup>
-            <tbody>
-              <tr>
-                <td class="title">이용자</td>
-                <td>김하나</td>
-                <td class="title">계약번호</td>
-                <td>D12345-12345-11111</td>
-                <td class="title">할부원금</td>
-                <td>2,564,000원</td>
-              </tr>
-              <tr>
-                <td class="title">주민/사업번호</td>
-                <td>545-**-*****</td>
-                <td class="title">실행일자</td>
-                <td>2023.01.01</td>
-                <td class="title">취급수수료</td>
-                <td>999,999,999원</td>
-              </tr>
-              <tr>
-                <td class="title">상품종류</td>
-                <td>일반할부_일반할부</td>
-                <td class="title">할부기간</td>
-                <td>36개월</td>
-                <td class="title">유예원금</td>
-                <td>999,999,999원</td>
-              </tr>
-              <tr>
-                <td class="title">품목명</td>
-                <td colspan="5">카드/포스 단말기</td>
-              </tr>
-            </tbody>
-          </table>
 
-          <div class="title">
+      <section class="row-margin-block-small">
+        <section class="row-margin-block-small">
+          <h3 class="text-title-1 row-margin-contents">계약내용</h3>
+
+          <div :class="$style['basic-table']">
+            <table>
+              <colgroup>
+                <col width="13%" />
+                <col width="20%" />
+                <col width="13%" />
+                <col width="20%" />
+                <col width="13%" />
+                <col width="20%" />
+              </colgroup>
+              <tbody>
+                <tr>
+                  <td class="title">이용자</td>
+                  <td>김하나</td>
+                  <td class="title">계약번호</td>
+                  <td>D12345-12345-11111</td>
+                  <td class="title">할부원금</td>
+                  <td>2,564,000원</td>
+                </tr>
+                <tr>
+                  <td class="title">주민/사업번호</td>
+                  <td>545-**-*****</td>
+                  <td class="title">실행일자</td>
+                  <td>2023.01.01</td>
+                  <td class="title">취급수수료</td>
+                  <td>999,999,999원</td>
+                </tr>
+                <tr>
+                  <td class="title">상품종류</td>
+                  <td>일반할부_일반할부</td>
+                  <td class="title">할부기간</td>
+                  <td>36개월</td>
+                  <td class="title">유예원금</td>
+                  <td>999,999,999원</td>
+                </tr>
+                <tr>
+                  <td class="title">품목명</td>
+                  <td colspan="5">카드/포스 단말기</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </section>
+
+      <section class="row-margin-block-small">
+        <section class="row-margin-block-small container">
+          <h3 class="text-title-1 row-margin-contents title">
             상한스케줄
             <div class="btn-container">
               <button class="btn btn-s03">
@@ -88,18 +96,10 @@ export default {
                 인쇄하기
               </button>
             </div>
-          </div>
+          </h3>
 
-          <table class="table-type-01 none-search">
-            <tbody>
-              <tr>
-                <td>조회된 내용이 없습니다.</td>
-              </tr>
-            </tbody>
-          </table>
-
-          <div class="scroll-container over-h">
-            <table class="table-type-01">
+          <div :class="$style['basic-table']">
+            <table>
               <colgroup>
                 <col width="6%" />
                 <col width="9%" />
@@ -136,8 +136,16 @@ export default {
               </tbody>
             </table>
           </div>
-        </div>
+        </section>
       </section>
     </ModalPopup>
   </UiLayer>
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets_agent/scss/views/agent/AgentCommon.scss';
+</style>
+
+<style lang="scss" module>
+@import '@/assets_agent/scss/components/import/basic-table.scss';
+</style>
